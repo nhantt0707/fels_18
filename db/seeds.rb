57049fb2db47dc93065c15ba2a206553f148a31a
bravewolf <nguyentruongthinhcp@gmail.com>
2014-11-13 15:58:30 +0700

    word

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,3 +20,32 @@ following = users[2..50]
 followers = users[3..40]
 following.each { |followed| user.follow(followed) }
 followers.each { |follower| follower.follow(user) }
+
+10.times do |n|
+  name  = Faker::Lorem.sentence
+  Category.create! name:  name
+end
+
+100.times do |n|
+  english  = Faker::Lorem.word
+  category_id = n/10 + 1
+  Word.create!(english:  english,
+               category_id: category_id)
+end
+
+100.times do |n|
+  content  = Faker::Lorem.word
+  correct = true;
+  word_id = n+1;
+  Answer.create!(content:  content,
+                 correct: correct,
+                 word_id: word_id)
+
+  3.times do |m|
+    content  = Faker::Lorem.word
+    correct = false;
+    Answer.create!(content:  content,
+                   correct: correct,
+                   word_id: word_id)
+  end
+end
