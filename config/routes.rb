8d14af0c7f90ef82f11e3c05cb30682df66aa578
bravewolf <nguyentruongthinhcp@gmail.com>
2014-11-25 13:46:09 +0700

    category

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
+
   root "static_pages#home"
   get "signup"  => "users#new"
   get    "login"   => "sessions#new"
   post   "login"   => "sessions#create"
   delete "logout"  => "sessions#destroy"
   resources :users 
-  resources :words
+  resources :words, only: [:index]
+  resources :categories, only: [:index]
   resources :relationships, only: [:create, :destroy]
   match "/users/:id/:follow_type", as: "follow_user", to: "users#index", via: "get"
 
